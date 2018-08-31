d79f0859142328be32318e17b17cc1501f8a5d8a
Abraham Howland <abe@flipstone.com>
2018-08-31 14:30:09 -0500

    liminalisht/semigroupifdefsbase (#32)
    * Adds ifdef statements that supply Semigroup instances if the version of base is >= 4.11.0

--- src/Database/Orville/Internal/Expr/Expr.hs
+++ src/Database/Orville/Internal/Expr/Expr.hs
@@ -3,6 +3,8 @@ Module    : Database.Orville.Internal.Expr.Expr
 Copyright : Flipstone Technology Partners 2016-2018
 License   : MIT
 -}
+{-#LANGUAGE CPP#-}
+
 module Database.Orville.Internal.Expr.Expr where
 
 import Data.String
@@ -22,6 +24,11 @@ rawExprToSql = go ""
     go rest (RawExprString s) = s ++ rest
     go rest (RawExprAppend r1 r2) = go (go rest r2) r1
     go rest (RawExprConcat exprs) = foldr (flip go) rest exprs
+
+#if MIN_VERSION_base(4,11,0)
+instance Semigroup RawExpr where
+  (<>) = RawExprAppend
+#endif
 
 instance Monoid RawExpr where
   mempty = RawExprString ""
