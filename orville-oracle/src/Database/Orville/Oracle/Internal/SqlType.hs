2647dc9998cda0dc76e867620a7a61e009173c13
Trevis <trevis@flipstone.com>
2019-08-13 07:46:22 -0400

    Decode double fields from bytestring

--- orville-oracle/src/Database/Orville/Oracle/Internal/SqlType.hs
+++ orville-oracle/src/Database/Orville/Oracle/Internal/SqlType.hs
@@ -343,6 +343,7 @@ doubleFromSql :: HDBC.SqlValue -> Maybe Double
 doubleFromSql sql =
   case sql of
     HDBC.SqlDouble d -> Just d
+    HDBC.SqlByteString n -> (readMaybe . B8.unpack) n
     _ -> Nothing
 
 booleanToSql :: Bool -> HDBC.SqlValue
