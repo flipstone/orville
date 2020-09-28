1e963bc22b0f31e0255ea815a3458e94fba3aff4
Trevis Elser <trevis@flipstone.com>
2020-09-28 13:00:31 -0400

    Update comment text

--- orville-postgresql-libpq/src/Database/Orville/PostgreSQL/Connection.hs
+++ orville-postgresql-libpq/src/Database/Orville/PostgreSQL/Connection.hs
@@ -36,7 +36,6 @@ createConnectionPool stripes linger maxRes connectionString =
 -}
 newtype Connection = Connection (MVar LibPQ.Connection)
 
-
 {-|
  `connect` is the internal, primitive connection function.
  This should not be exposed to end users, but instead wrapped in something to create a pool.
@@ -69,7 +68,7 @@ connect connectionString = do
 {-|
   `close` has many subtleties to it.
   First note that async exceptions are masked.
-  `mask` though, only works for things that not interruptible <https://www.stackage.org/haddock/lts-16.15/base-4.13.0.0/Control-Exception.html#g:13>
+  `mask` though, only works for things that are not interruptible <https://www.stackage.org/haddock/lts-16.15/base-4.13.0.0/Control-Exception.html#g:13>
   From the previous link, `tryTakeMVar` is not interruptible, where `takeMVar` *is*.
   So by using `tryTakeMVar` along with `mask`, we should be safe from async exceptions causing us to not finish an underlying connection.
   Notice that the only place the MVar is ever taken is here so `tryTakeMVar` gives us both the non-blocking semantics to protect from async exceptions with `mask` _and_ should never truly return an empty unless two threads were racing to close the connection, in which case.. one of them will close the connection.
