2e4f3345c6d149427a7e6d3c6f0b06ba8cc8bbd3
Trevis Elser <trevis@flipstone.com>
2021-01-28 17:50:31 -0500

    Adds the beginning of a test suite Moving the previous visual inspection code to a "real" runnable test suite Adds test looping and docker-y bits adapted from "main" orville code Also updates internal representations to be all based on ByteString

--- orville-postgresql-libpq/src/Database/Orville/PostgreSQL/Internal/Expr.hs
+++ orville-postgresql-libpq/src/Database/Orville/PostgreSQL/Internal/Expr.hs
@@ -20,44 +20,40 @@ module Database.Orville.PostgreSQL.Internal.Expr
   , insertExprToSql
   ) where
 
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as TextEnc
 import qualified Data.ByteString.Char8 as B8
 
 -- This is a rough model of "query specification" see https://jakewheat.github.io/sql-overview/sql-2016-foundation-grammar.html#_7_16_query_specification for more detail than you probably want
 data QueryExpr = QueryExpr
-  { selectList :: [T.Text]
+  { selectList :: [B8.ByteString]
   , tableExpression :: TableExpr
   }
 
 data TableExpr = TableExpr
-  { fromClause :: T.Text
+  { fromClause :: B8.ByteString
 -- where, groupby, having, etc
   }
 
 queryExprToSql :: QueryExpr -> B8.ByteString
 queryExprToSql queryExpr =
-  TextEnc.encodeUtf8 $
-    T.concat
-      [ T.pack "SELECT "
-      , T.intercalate (T.pack ",") (selectList queryExpr)
-      , T.pack " FROM "
-      , fromClause (tableExpression queryExpr)
-      ]
+  B8.concat
+    [ B8.pack "SELECT "
+    , B8.intercalate (B8.pack ",") (selectList queryExpr)
+    , B8.pack " FROM "
+    , fromClause (tableExpression queryExpr)
+    ]
 
 data InsertExpr =
   InsertExpr
-    { target :: T.Text
-    , rowValues :: [T.Text]
+    { target :: B8.ByteString
+    , rowValues :: [B8.ByteString]
     }
 
 insertExprToSql :: InsertExpr -> B8.ByteString
 insertExprToSql insertExpr =
-  TextEnc.encodeUtf8 $
-    T.concat
-      [ T.pack "INSERT INTO "
-      , target insertExpr
-      , T.pack " VALUES ("
-      , T.intercalate (T.pack ",") (rowValues insertExpr)
-      , T.pack ")"
-      ]
+  B8.concat
+    [ B8.pack "INSERT INTO "
+    , target insertExpr
+    , B8.pack " VALUES ("
+    , B8.intercalate (B8.pack ",") (rowValues insertExpr)
+    , B8.pack ")"
+    ]
