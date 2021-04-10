1a3c7ebd7590e9bb73e9eb8afcb533dd01ea3e29
David Vollbracht <david.vollbracht@gmail.com>
2021-04-10 12:56:07 -0500

    Adds Database.Orville.PostgreSQL.Plan for planning queries
    This adds a new 'Plan' type that is meant to be the replacement for
    'Popper' in the eventual libpq based Hackage release. Rather than using
    'Arrow' as the primary interface for chaining queries together, 'Plan'
    provides 'bind' function that is quite similar the 'Monad' bind function
    '>>=', though does not follow the exact same type signature. Although
    'Plan' cannot implement 'Monad', having 'bind' function will enable the
    use of GHC 9 'QualifiedDo' extension.
    
    A future PR will provide the interface required for the extension along
    with examples. This PR is introduces the type so that we can start using
    it before GHC9 is more available.

--- orville-postgresql/test/PlanTest.hs
+++ orville-postgresql/test/PlanTest.hs
@@ -1,38 +1,75 @@
-module PopperTest where
+{-# LANGUAGE RankNTypes #-}
+module PlanTest
+  ( test_plan
+  ) where
 
-import Control.Monad (void)
-import Data.Int (Int32)
+import           Control.Monad (void)
+import           Data.Function (on)
+import           Data.Int (Int32)
 import qualified Data.List as List
 import qualified Data.Map as Map
-import Data.Ratio ((%))
+import           Data.Ratio ((%))
 import qualified Data.Set as Set
-import Data.Word (Word8)
-import qualified Database.Orville.PostgreSQL as O
-import Database.Orville.PostgreSQL.Popper ((>>>))
-import qualified Database.Orville.PostgreSQL.Popper as P
+import           Data.Word (Word8)
 
-import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (assertEqual, testCase)
-import Test.Tasty.QuickCheck (Arbitrary(arbitrary), testProperty)
-import Test.Tasty.QuickCheck as QC
+import qualified Database.Orville.PostgreSQL as O
+import qualified Database.Orville.PostgreSQL.Plan as Plan
+import qualified Database.Orville.PostgreSQL.Plan.Many as Many
+
+import           Test.Tasty (TestTree, testGroup)
+import qualified Test.Tasty.HUnit as HUnit
+import qualified Test.Tasty.QuickCheck as QC
 
 import qualified TestDB as TestDB
 
-test_popper :: TestTree
-test_popper =
-  testGroup
-    "PopperTest"
-    [ testCase "explainLines works" $
-      let actual = P.explainLines treeRootPopper
+test_plan :: TestTree
+test_plan =
+  TestDB.withOrvilleRun $ \run ->
+    testGroup
+      "PlanTest"
+      [ HUnit.testCase "explain works" $
+        let
+          actual = Plan.explain treePlan
           expected =
-            [ "fetch one root by one id "
-            , "fetch many branch by one tree_id "
-            , "fetch many leaf by many branch_id (*-1) "
+            [ "SELECT \"id\", \"id\", \"tree_id\" FROM \"root\" WHERE (\"id\" = ?)    "
+            , "SELECT \"tree_id\", \"id\", \"tree_id\" FROM \"branch\" WHERE (\"tree_id\" = ?)    "
+            , "SELECT \"branch_id\", \"id\", \"branch_id\" FROM \"leaf\" WHERE (\"branch_id\" IN (?,?))    "
             ]
-       in assertEqual "Expected explanation to match" expected actual
+         in
+           HUnit.assertEqual "Expected explanation to match" expected actual
 
-    , TestDB.withOrvilleRun $ \run ->
-        testProperty "treeRootPopper" $ \expected ->
+      , testRootPlan "findMaybeOne" run $
+          Plan.findMaybeOne rootTable rootIdField
+
+      , testRootPlan "bind, chain, findMaybeOne" run $
+          Plan.bind Plan.askParam $ \param ->
+            Plan.chain
+              (Plan.use param)
+              (Plan.findMaybeOne rootTable rootIdField)
+
+      , QC.testProperty "many findAll includes all params in map" $ \rootIds ->
+        QC.ioProperty $ do
+          actual <-
+            run $ do
+              TestDB.reset testSchema
+              Many.toMap <$>
+                Plan.execute
+                  (Plan.planMany (Plan.findAll rootTable rootIdField))
+                  rootIds
+
+          let
+            expected :: Map.Map RootId [Root]
+            expected =
+              Map.fromList $
+                map (\rId -> (rId, [])) rootIds
+
+          pure $
+            QC.counterexample ("Expected: " ++ show expected) $
+            QC.counterexample ("Actual: " ++ show actual) $
+            (expected == actual)
+
+
+      , QC.testProperty "treePlan" $ \expected ->
           let (root, branches, leaves) = mkTreeRecords expected
               leafRatio = (length leaves + 1) % (length branches + 1)
            in QC.ioProperty $ do
@@ -42,30 +79,16 @@ test_popper =
                     void (O.insertRecord rootTable root)
                     void (O.insertRecordMany branchTable branches)
                     void (O.insertRecordMany leafTable leaves)
-                    P.popThrow treeRootPopper (treeRootId expected)
+                    Plan.execute treePlan (treeRootId expected)
                 pure $
                   QC.classify (length branches > 1) "Num branches > 1" $
                   QC.classify (leafRatio > 1) "Avg leafs/branch > 1" $
-                  QC.classify
-                    (length branches == 1)
-                    "Trivial (Num branches == 1)" $
+                  QC.classify (length branches == 1) "Trivial (Num branches == 1)" $
                   QC.classify (null branches) "Trivial (Num branches == 0)" $
-                  QC.counterexample
-                    ("Actual: " ++ show actual)
-                    (expected == actual)
-    , TestDB.withOrvilleRun $ \run ->
-        testProperty "popMany pure doesn't cause infinite loop" $ \count -> do
-          let roots1Pooper =
-                P.popMany (pure (RootId 1) >>> P.hasOne rootTable rootIdField)
-              inputs = replicate count ()
-              expected = replicate count Nothing
-          QC.ioProperty $ do
-            actual <-
-              run $ do
-                TestDB.reset testSchema
-                P.popThrow roots1Pooper inputs
-            pure (expected == actual)
-    ]
+                  QC.counterexample ("Expected: " ++ show expected) $
+                  QC.counterexample ("Actual: " ++ show actual) $
+                  (expected == actual)
+      ]
 
 -- mkTreeRecords projects a Tree into the Root, Branch and Leaf entities
 -- that should be recorded in the database to reflect the structure of
@@ -83,24 +106,64 @@ mkTreeRecords tree = (root, branches, concat leafLists)
           leaves = map (flip Leaf bId) (Set.toList lfIds)
        in (branch : bAccum, leaves : lAccum)
 
-treeRootPopper :: P.Popper RootId Tree
-treeRootPopper = P.hasOne' rootTable rootIdField >>> rootPopper
 
-rootPopper :: P.Popper Root Tree
-rootPopper =
-  Tree <$> P.fromKern rootTreeId <*> P.fromKern rootId <*>
-  (P.fromKern rootTreeId >>>
-   P.hasMany branchTable treeIdField >>>
-   P.popMany branchPopper >>> P.fromKern Map.fromList)
+testRootPlan :: String
+             -> TestDB.OrvilleRunner
+             -> (forall scope. Plan.Plan scope RootId (Maybe Root))
+             -> TestTree
+testRootPlan groupName run plan =
+  testGroup groupName
+    [ QC.testProperty "One" $ \expected ->
+        QC.ioProperty $ do
+          actual <- run $ do
+            TestDB.reset testSchema
+            void (O.insertRecord rootTable expected)
+            Plan.execute plan (rootId expected)
 
-branchPopper :: P.Popper Branch (BranchId, Set.Set LeafId)
-branchPopper =
-  pair <$> P.fromKern branchId <*>
-  (Set.fromList <$>
-   (P.fromKern branchId >>>
-    P.hasMany leafTable branchForeignIdField >>> P.fromKern (map leafId)))
-  where
-    pair x y = (x, y)
+          pure $
+            QC.counterexample ("Expected: " ++ show expected) $
+              QC.counterexample ("Actual: " ++ show actual) $
+                (Just expected == actual)
+
+    , QC.testProperty "Many" $ \roots ->
+        QC.ioProperty $ do
+          let
+            uniqRoots =
+              List.nubBy ((==) `on` rootId) roots
+
+            expecteds =
+              Map.fromList $
+                map (\root -> (rootId root, Just root)) uniqRoots
+
+          actuals <- run $ do
+            TestDB.reset testSchema
+            void (O.insertRecordMany rootTable uniqRoots)
+            Many.toMap <$>
+              Plan.execute
+                (Plan.planMany plan)
+                (rootId <$> uniqRoots)
+
+
+          pure $
+            QC.counterexample ("Expected: " ++ show expecteds) $
+              QC.counterexample ("Actual: " ++ show actuals) $
+                (expecteds == actuals)
+    ]
+
+treePlan :: Plan.Plan scope RootId Tree
+treePlan =
+  Plan.bind (Plan.findOne rootTable rootIdField) $ \root ->
+  Plan.bind (Plan.using (rootTreeId <$> root) (Plan.findAll branchTable treeIdField)) $ \branches ->
+  Plan.bind (Plan.using (map branchId <$> branches) (Plan.planMany branchPlan)) $ \branchMaps ->
+    Tree
+      <$> Plan.use (rootTreeId <$> root)
+      <*> Plan.use (rootId <$> root)
+      <*> Plan.use (Many.toMap <$> branchMaps)
+
+branchPlan :: Plan.Plan scope BranchId (Set.Set LeafId)
+branchPlan =
+  Set.fromList . map leafId <$>
+    Plan.findAll leafTable branchForeignIdField
 
 data Tree = Tree
   { treeId :: TreeId
@@ -113,10 +176,14 @@ data Root = Root
   , rootTreeId :: TreeId
   } deriving (Eq, Show)
 
+instance QC.Arbitrary Root where
+  arbitrary =
+    Root <$> QC.arbitrary <*> QC.arbitrary
+
 data Branch = Branch
   { branchId :: BranchId
   , branchTreeId :: TreeId
-  } deriving (Eq, Show)
+  } deriving (Eq, Ord, Show)
 
 data Leaf = Leaf
   { leafId :: LeafId
@@ -125,19 +192,19 @@ data Leaf = Leaf
 
 newtype RootId = RootId
   { rootIdToInt :: Int32
-  } deriving (Eq, Ord, Show, Arbitrary)
+  } deriving (Eq, Ord, Show, QC.Arbitrary)
 
 newtype TreeId = TreeId
   { treeIdToInt :: Int32
-  } deriving (Eq, Ord, Show, Arbitrary)
+  } deriving (Eq, Ord, Show, QC.Arbitrary)
 
 newtype BranchId = BranchId
   { branchIdToInt :: Int32
-  } deriving (Eq, Ord, Show, Arbitrary)
+  } deriving (Eq, Ord, Show, QC.Arbitrary)
 
 newtype LeafId = LeafId
   { leafIdToInt :: Int32
-  } deriving (Eq, Ord, Show, Arbitrary)
+  } deriving (Eq, Ord, Show, QC.Arbitrary)
 
 idField :: O.FieldDefinition O.NotNull Int32
 idField = O.int32Field "id"
@@ -204,11 +271,11 @@ leafTable =
     , O.tblComments = O.noComments
     }
 
-instance Arbitrary Tree where
+instance QC.Arbitrary Tree where
   arbitrary =
-    mkTree <$> arbitrary <*> arbitrary <*> arbitraryLeafCounts <*>
-    (List.nub <$> infiniteListOf arbitrary) <*>
-    (List.nub <$> infiniteListOf arbitrary)
+    mkTree <$> QC.arbitrary <*> QC.arbitrary <*> arbitraryLeafCounts <*>
+    (List.nub <$> QC.infiniteListOf QC.arbitrary) <*>
+    (List.nub <$> QC.infiniteListOf QC.arbitrary)
 
 type LeafCount = Word8
 
@@ -218,7 +285,7 @@ arbitraryLeafCounts
   -- from the `classify` cases listed in the popper test above to generate
   -- small enough test cases for the tests to run efficiently without only
   -- running trivial cases.
- = QC.scale (`div` 3) (QC.listOf (QC.scale (`div` 4) arbitrary))
+ = QC.scale (`div` 3) (QC.listOf (QC.scale (`div` 4) QC.arbitrary))
 
 mkTree :: TreeId -> RootId -> [LeafCount] -> [BranchId] -> [LeafId] -> Tree
 mkTree trId rtId leafCounts brIds lfIds = Tree trId rtId branches
