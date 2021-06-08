acb310571ac7f8bbd35240dc5974923c27b2bc95
Trevis <trevis@silencedpoet.com>
2021-06-08 18:23:45 -0400

    Removes dead Oracle port

--- orville-oracle/.ide.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-stack install --resolver lts-11.2 hindent
-
-echo 'IDE Services Running'
-exec cat
--- orville-oracle/test/EntityWrapper/Data/Entity.hs
+++
@@ -1,8 +0,0 @@
-module EntityWrapper.Data.Entity
-  ( Entity(..)
-  ) where
-
-data Entity key fields = Entity
-  { entityKey :: key
-  , entityFields :: fields
-  } deriving (Show, Eq)
