8f5aec5e00b4ed9bd01e00c0adb8cedd50c731fe
Janus Troelsen <janus@flipstone.com>
2023-02-28 16:00:49 -0600

    Decode seconds in UTC offset

--- orville-postgresql-libpq/test/Test/PgTime.hs
+++ orville-postgresql-libpq/test/Test/PgTime.hs
@@ -18,9 +18,15 @@ pgTimeTests :: Pool.Pool Conn.Connection -> Property.Group
 pgTimeTests _pool =
   Property.group "PgTime" $
     [
-      ( String.fromString "Ignores seconds in UTC offset correctly"
+      ( String.fromString "Handles seconds in UTC offset correctly"
       , Property.singletonProperty $
           parseOnly PgTime.utcTime (String.fromString "1971-01-01 00:00:00-00:44:30")
-            HH.=== Right (UTCTime (fromGregorian 1971 1 1) (44 * 60))
+            HH.=== Right (UTCTime (fromGregorian 1971 1 1) (44 * 60 + 30))
+      )
+    , ( String.fromString "Handles far future"
+      , Property.singletonProperty $
+          -- https://github.com/postgres/postgres/blob/b5737efea00717173c0cc889ebd115966abd8c8c/src/test/regress/sql/timestamptz.sql#L175
+          parseOnly PgTime.utcTime (String.fromString "294276-12-31 23:59:59+00")
+            HH.=== Right (UTCTime (fromGregorian 294276 12 31) (23 * 3600 + 59 * 60 + 59))
       )
     ]
