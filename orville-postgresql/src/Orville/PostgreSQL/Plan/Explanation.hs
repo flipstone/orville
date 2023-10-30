7ec346cc9de306ee245351dee8389f9bf79f016e
David Vollbracht <david@flipstone.com>
2023-10-30 08:32:17 -0500

    Adds docs for `Orville.PostgreSQL.Plan.Explanation`

--- orville-postgresql/src/Orville/PostgreSQL/Plan/Explanation.hs
+++ orville-postgresql/src/Orville/PostgreSQL/Plan/Explanation.hs
@@ -13,6 +13,12 @@ module Orville.PostgreSQL.Plan.Explanation
   )
 where
 
+{- |
+ An 'Explanation' represents an example sequence of queries showing the steps
+ would be executed by an Orville 'Orville.PostgreSQL.Plan.Operation.Operation'.
+
+@since 1.0.0.0
+-}
 newtype Explanation
   = Explanation ([String] -> [String])
 
@@ -22,18 +28,39 @@ instance Semigroup Explanation where
 instance Monoid Explanation where
   mempty = noExplanation
 
+{- |
+Appends two 'Explanation's with the steps from the first argument being shown
+first.
+
+@since 1.0.0.0
+-}
 appendExplanation :: Explanation -> Explanation -> Explanation
 appendExplanation (Explanation front) (Explanation back) =
   Explanation (front . back)
 
+{- |
+Constructs an empty 'Explanation'.
+
+@since 1.0.0.0
+-}
 noExplanation :: Explanation
 noExplanation =
   Explanation id
 
+{- |
+Constructs an 'Explanation' with a single step.
+
+@since 1.0.0.0
+-}
 explainStep :: String -> Explanation
 explainStep str =
   Explanation (str :)
 
+{- |
+Retrieves the steps contained in the 'Explanation'.
+
+@since 1.0.0.0
+-}
 explanationSteps :: Explanation -> [String]
 explanationSteps (Explanation prependTo) =
   prependTo []
